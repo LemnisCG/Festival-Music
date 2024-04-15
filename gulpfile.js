a2b3906a070b66ec14cae1bd991f2caa65eb336a
JChigne <lemniscatachigne@gmail.com>
2024-04-15 17:31:24 -0400

    Automatizando tareas con gulp

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const {src,dest,watch, parallel} = require("gulp"); //extrae funcion de gulp
+const sass = require("gulp-sass")(require("sass")); //extreae funcion de sass
+//pipe es una accion que se repite una de tras de otras
+function css(cb) {
+    src("src/scss/app.scss")//Identifica archivo dee sass
+        .pipe(sass())//compila la funcion de sass
+        .pipe(dest("build/css")) //Almacena en el disco duro
+
+    cb(); //Callback que avisa a gulp cuando llegamos al final
+}
+
+function dev(cb){
+    watch("src/scss/app.scss", css)
+    
+    cb();
+}
+
+
+exports.css = css
+exports.dev = parallel(css,dev)
