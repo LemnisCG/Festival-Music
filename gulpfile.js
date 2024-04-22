cea49ca17bdc5b7ca36b208b59afab38b3f3619c
JChigne <lemniscatachigne@gmail.com>
2024-04-22 18:27:43 -0400

    Añadiendo plumber a package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const {src,dest,watch, parallel} = require("gulp"); //extrae funcion de gulp
 const sass = require("gulp-sass")(require("sass")); //extreae funcion de sass
+const plumber = require("gulp-plumber");
 //pipe es una accion que se repite una de tras de otras
 function css(cb) {
     src("src/scss/*/**.scss")//Identifica archivo dee sass
+        .pipe(plumber())
         .pipe(sass())//compila la funcion de sass
         .pipe(dest("build/css")) //Almacena en el disco duro
 
