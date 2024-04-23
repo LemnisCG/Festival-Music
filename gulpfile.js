d21f3f1679eb926397521de8a22cf4c1b4ec262d
JChigne <lemniscatachigne@gmail.com>
2024-04-23 09:02:37 -0400

    Modificando gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,16 @@ const sass = require("gulp-sass")(require("sass")); //extreae funcion de sass
 const plumber = require("gulp-plumber");
 //pipe es una accion que se repite una de tras de otras
 function css(cb) {
-    src("src/scss/*/**.scss")//Identifica archivo dee sass
+    src('src/scss/**/*.scss')//Identifica archivo dee sass
         .pipe(plumber())
         .pipe(sass())//compila la funcion de sass
-        .pipe(dest("build/css")) //Almacena en el disco duro
+        .pipe(dest('build/css')) //Almacena en el disco duro
 
     cb(); //Callback que avisa a gulp cuando llegamos al final
 }
 
 function dev(cb){
-    watch("src/scss/*/**.scss", css)
+    watch("src/scss/**/*.scss", css)
     
     cb();
 }
